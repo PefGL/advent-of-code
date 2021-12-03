f49799f6189194f863fe9e771fc6faa5f886afda
LFCP <pierre-francois.lefranc@cnieg.fr>
2021-12-03 15:27:41 +0100

    Day2 part 1

--- src/test/java/DiveServiceTest.java
+++ src/test/java/DiveServiceTest.java
@@ -29,7 +29,7 @@ public class DiveServiceTest {
     }
 
     @Test
-    public void position_retourne_1_quand_profondeur_1_et_horizontal_1() throws IOException {
+    public void position_retourne_1_quand_descendre_1_et_avancer_1() throws IOException {
         int attendu = 1;
         File inputs = TestUtil.construireInput("forward 1","down 1");
 
@@ -37,4 +37,23 @@ public class DiveServiceTest {
 
         Assert.assertEquals(attendu, position);
     }
+
+    @Test
+    public void position_retourne_150_quand_avancer_5_descendre_5_avancer_8_monter_3_descendre_8_avancer_2() throws IOException {
+        int attendu = 150;
+        File inputs = TestUtil.construireInput("forward 5","down 5","forward 8","up 3", "down 8", "forward 2");
+
+        int position = diveService.recupererPosition(inputs);
+
+        Assert.assertEquals(attendu, position);
+    }
+
+
+
+
+
+
+
+
+
 }
